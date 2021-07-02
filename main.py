0eaf9a758756335697b5a556093c663e5c570ca8
StickyMouse <x00001101@gmail.com>
2021-07-02 21:40:39 +0700

    adding spritesheet class

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
 import pygame, sys
+from pygame import sprite
 from pygame.locals import *
 from source.variable import *
+from source.spritesheet import *
 
 # Initialize program
 pygame.init()
  
 # Assign FPS a value
 FPS = 60
-FramePerSec = pygame.time.Clock()
+clock = pygame.time.Clock()
  
 screen = pygame.display.set_mode((500,500))
 pygame.display.set_caption("MyGame")
- 
+
+# display 
+spritesheet = spritesheet(PLAYER_IMAGE[0])
+
+PLAYER_SPRITE = spritesheet.get_image(32, 32, 2)
+
 # Beginning Game Loop
-while True:
-    screen.fill(LIGHTSLATEGREY)
-    pygame.display.update()
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-   
-    FramePerSec.tick(FPS)
+def main():
+
+    run = True
+    while run:
+        clock.tick(FPS)
+        screen.fill(LIGHTSLATEGREY)
+        screen.blit(PLAYER_SPRITE, (10,10))
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                run = False
+    
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
