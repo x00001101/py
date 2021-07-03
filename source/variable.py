008b0669bfe01d66c45e555dbd023fe3d055640c
StickyMouse <x00001101@gmail.com>
2021-07-03 22:29:30 +0700

    remove images from variable because why not

--- source/variable.py
+++ source/variable.py
@@ -4,13 +4,7 @@ import pygame
 # color variables
 BLACK = (0,0,0)
 WHITE = (255,255,255)
-DARKSLATEGREY  = (47,79,79)
+DARKSLATEGREY = (47,79,79)
 LIGHTSLATEGREY = (119,136,153)
 DIMGREY = (105,105,105)
 GREY = (128,128,128)
-
-# images
-PLAYER_IMAGE = []
-PLAYER_SPRITE_NAME = 'knight_m_idle_anim_f' # this is default character
-for frame in range(0,3):
-    PLAYER_IMAGE.append(pygame.image.load(os.path.join('assets', 'frames', PLAYER_SPRITE_NAME + str(frame) +'.png')))
